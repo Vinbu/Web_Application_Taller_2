fe1e937a10977103e7544854a3e191d60e5281ba
AlanSilvaaa <asilvar4@correo.uss.cl>
2025-03-25 13:37:23 -0300

    linter implementation

--- src/main.py
+++ src/main.py
@@ -4,47 +4,47 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
 app = typer.Typer()
-iv = b'3afMoPuzpXwkdoOO' # default iv to encrypt and decrypt
+iv = b'3afMoPuzpXwkdoOO'  # default iv to encrypt and decrypt
+
 
 @app.command()
 def encrypt(message: str, key: str) -> str:
     """
-    encrypt function will encrypt the message using the key provided and global iv
-    
+    encrypt function will encrypt the message using the key providedddddddddddddddddddddddddddddddddddddddddddddddddddd
+    and global iv
+
     Parameters:
         message (str): Message to be encrypted
         key (str): Key to encrypt the message
     """
-    
     if len(key) != 16:
         raise ValueError("The key must be exactly 16 characters length")
-    
+
     # parse to bytes
     key = key.encode()
     message = message.encode()
-    
     # encrypt the message
     cipher = AES.new(key, AES.MODE_CBC, iv)
     ct_bytes = cipher.encrypt(pad(message, AES.block_size))
     ciphertext = b64encode(ct_bytes).decode('utf-8')
-    
     print(f"Encrypted Message: {ciphertext}")
     return ciphertext
-    
+
+
 @app.command()
 def decrypt(encode_message: str, key: str) -> str:
     """
-    decrypt function will decrypt the message using the key provided, encrypted message and global iv
+    decrypt function will decrypt the message using the key provided,
+    encrypted message and global iv
 
     Parameters:
         encode_message (str): Encrypted message to be decrypted
         key (str): Key to decrypt the message
     """
-    
+
     try:
         if len(key) != 16:
             raise ValueError("The key must be exactly 16 characters length")
-        
         # parse to bytes
         key = key.encode()
         ct = b64decode(encode_message)
@@ -58,6 +58,7 @@ def decrypt(encode_message: str, key: str) -> str:
 
     except (ValueError, KeyError):
         print("Incorrect decryption")
-    
+
+
 if __name__ == "__main__":
     app()
--- src/test_main.py
+++ src/test_main.py
@@ -1,17 +1,19 @@
 from main import encrypt, decrypt
 import pytest
 
+
 def test_encrypt():
     encrypted_message = encrypt("Test message", "1234567890123456")
     assert encrypted_message == "YRNFocfXi5FBg0oR6TEjtg==", "Message not encrypted correctly"
-    
+
+
 def test_decrypt():
     message = decrypt("YRNFocfXi5FBg0oR6TEjtg==", "1234567890123456")
     assert message == "Test message", "Message not decrypted correctly"
 
+
 def test_invalid_key_length():
     message = "Test message"
     invalid_key = "shortkey"
-    
     with pytest.raises(ValueError, match="The key must be exactly 16 characters length"):
         encrypt(message, invalid_key)
